a23dd2394b768b3779e1766404116b18e36b2d02
Arsen <akolintchuk@gmail.com>
2026-04-22 12:58:35 +0300

    git init git add . git commit -m "Initial commit"

---
+++ src/HelloWorldApp.java
@@ -0,0 +1,24 @@
+/**
+ * Main class of the program.
+ * <p>
+ * This class contains the entry point of the application and prints
+ * the message "Hello World" to the console.
+ * </p>
+ *
+ * @author Arsen
+ * @version 1.0
+ */
+public class HelloWorldApp {
+
+    /**
+     * The entry point of the program.
+     * <p>
+     * This method prints the string "Hello World" to the standard output.
+     * </p>
+     *
+     * @param args command-line arguments (not used in this program)
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+    }
+}
